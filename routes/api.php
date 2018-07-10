ebbd6a746f0d5b6ca5da799eb48b1f4c49a0e487
Achmad Fajar Ridwan <achmadfajar.ridwan@mitrais.com>
2018-07-10 23:18:05 +0700

    Install API authentication

--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('login', 'UserController@login');
 Route::post('tenants/register', 'TenantController@register');
